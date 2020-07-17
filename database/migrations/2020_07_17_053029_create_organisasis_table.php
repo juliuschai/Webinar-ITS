38169c76dd4cf8dbeaec477d6a11cccd18a6ee74
Julius <juliustjai20@gmail.com>
2020-07-17 21:40:47 +0700

    Updated migrations
    migrations now consist of newer departments and faculties

--- database/migrations/2020_07_17_053029_create_organisasis_table.php
+++ database/migrations/2020_07_17_053029_create_organisasis_table.php
@@ -22,10 +22,50 @@ class CreateOrganisasisTable extends Migration
         });
 
         DB::table('organisasis')->insert([
-            ['nama' => 'Fakultas Teknologi Kelautan', 'org_type_id' => 2],
+            ['nama' => "Fakultas Sains dan Analitika Data", 'org_type_id' => 2],
+            ['nama' => "Fisika", 'org_type_id' => 1],
+            ['nama' => "Matematika", 'org_type_id' => 1],
+            ['nama' => "Statistika", 'org_type_id' => 1],
+            ['nama' => "Kimia", 'org_type_id' => 1],
+            ['nama' => "Biologi", 'org_type_id' => 1],
+            ['nama' => "Aktuaria", 'org_type_id' => 1],
 
-            ['nama' => 'Informatika', 'org_type_id' => 1],
+            ['nama' => "Fakultas Teknologi Industri dan Rekayasa Sistem", 'org_type_id' => 2],
+            ['nama' => "Teknik Mesin", 'org_type_id' => 1],
+            ['nama' => "Teknik Kimia", 'org_type_id' => 1],
+            ['nama' => "Teknik Fisika", 'org_type_id' => 1],
+            ['nama' => "Teknik Industri", 'org_type_id' => 1],
+            ['nama' => "Teknik Material", 'org_type_id' => 1],
 
+            ['nama' => "Fakultas Teknik Sipil, Perencanaan dan Kebumian", 'org_type_id' => 2],
+            ['nama' => "Teknik Sipil", 'org_type_id' => 1],
+            ['nama' => "Arsitektur", 'org_type_id' => 1],
+            ['nama' => "Teknik Lingkungan", 'org_type_id' => 1],
+            ['nama' => "Perencanaan Wilayah Kota", 'org_type_id' => 1],
+            ['nama' => "Teknik Geomatika", 'org_type_id' => 1],
+            ['nama' => "Teknik Geofisika", 'org_type_id' => 1],
+
+            ['nama' => "Fakultas Teknologi Kelautan", 'org_type_id' => 2],
+            ['nama' => "Teknik Perkapalan", 'org_type_id' => 1],
+            ['nama' => "Teknik Sistem Perkapalan", 'org_type_id' => 1],
+            ['nama' => "Teknik Kelautan", 'org_type_id' => 1],
+            ['nama' => "Teknik Transportasi Laut", 'org_type_id' => 1],
+
+            ['nama' => "Fakultas Teknologi Elektro dan Informatika Cerdas", 'org_type_id' => 2],
+            ['nama' => "Teknik Elektro", 'org_type_id' => 1],
+            ['nama' => "Teknik Biomedik", 'org_type_id' => 1],
+            ['nama' => "Teknik Komputer", 'org_type_id' => 1],
+            ['nama' => "Teknik Informatika", 'org_type_id' => 1],
+            ['nama' => "Sistem Informasi", 'org_type_id' => 1],
+            ['nama' => "Teknologi Informasi", 'org_type_id' => 1],
+
+            ['nama' => "Fakultas Desain Kreatif dan Bisnis Digital", 'org_type_id' => 2],
+            ['nama' => "Desain Produk Industri", 'org_type_id' => 1],
+            ['nama' => "Desain Interior", 'org_type_id' => 1],
+            ['nama' => "Desain Komunikasi Visual", 'org_type_id' => 1],
+            ['nama' => "Manajemen Bisnis", 'org_type_id' => 1],
+            ['nama' => "Studi Pembangunan", 'org_type_id' => 1],
+            
             ['nama' => "Kantor Penjaminan Mutu", 'org_type_id' => 3],
             ['nama' => "Kantor Audit Internal", 'org_type_id' => 3],
             ['nama' => "Sekretaris Institut", 'org_type_id' => 3],
