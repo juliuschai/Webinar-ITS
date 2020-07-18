01a34bfc94e91693dcb2ce69b983ac12a4cb0ff4
hipzi <zahramillah582@gmail.com>
2020-07-18 10:15:40 +0700

    admin view

---
+++ resources/views/admin/table.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    @if($isAdmin)
+	<div class="right_col booking" role="main">
+		<div class="col-md-12 col-sm-12">
+            <h2 class="table-title">Waiting List Webinar</h2>
+            <table class="table table-bordered table-striped table-bordered table-hover">
+                <thead class="thead-custom-blue">
+                    <tr>
+                    <th class="text-center" scope="col">#</th>
+                    <th class="text-center" scope="col">Tanggal</th>
+                    <th class="text-center" scope="col">Waktu</th>
+                    <th class="text-center" scope="col">Nama Acara</th>
+                    <th class="text-center" scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach( $booking as $booking)
+                    <tr>
+                    <th class="text-center" scope="row">{{ $loop->iteration }}</th>
+                    <td class="text-center">{{ date('d-m-Y', strtotime($booking->waktu_mulai)) }}</td>
+                    <td class="text-center">{{ date('H:i:s', strtotime($booking->waktu_mulai)) }}</td>
+                    <td class="text-center">{{ $booking->nama_acara }}</td>
+                    <td class="text-center">
+                        <a href="{{ url('/admin/booking/view/'.$booking->id) }}">
+                        <button type="button" class="btn btn-custom-primary" title="Detail Webinar">
+                            <i class="fa fa-search"> Detail</i>
+                        </button>
+                        </a>
+                    </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+		</div>
+	</div>
+<!-- </div> -->
+<script defer>
+
+	function getTimeZoneOffsetInMs() {
+		return new Date().getTimezoneOffset() * -60 * 1000;
+	}
+	
+	function onupdateDurasi() {
+		let start = new Date(document.getElementById('waktuMulai').value);
+		let hours = document.getElementById('durasi').value;
+
+		let end = new Date(start.getTime() + getTimeZoneOffsetInMs() + parseFloat(hours)*3600*1000);
+		document.getElementById('waktuSelesai').value = end.toISOString().substring(0, 16);
+	}
+
+	function onupdateWaktu() {
+		let start = new Date(document.getElementById('waktuMulai').value);
+		let end = new Date(document.getElementById('waktuSelesai').value);
+
+		document.getElementById('durasi').value = (end-start)/3600/1000;
+	}
+	
+	// Even though the script is already at the bottom of the page and inputs are already loaded,
+	// Seems like the function still needs to wait until document is ready
+	// idk what's not ready when the script is loaded with the html tho
+	setTimeout(onupdateWaktu, 500);
+</script>
+    @endif
+@endsection
---
+++ resources/views/admin/view.blade.php
@@ -0,0 +1,291 @@
+@extends('layouts.admin')
+
+@section('content')
+
+	<div class="right_col booking" role="main">
+		<div class="col-md-12 col-sm-12">
+			<div class="card">
+				<div class="card-header">{{ __('Booking Form') }}</div>
+
+				<div class="card-body">
+					@if ($errors->any())
+					<div class="alert alert-danger">
+						<ul>
+							@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+					@endif
+
+					@if($isOwner || $isAdmin)
+					<div class="form-group row">
+						<label for="namaPic" class="col-md-4 col-form-label text-md-left">{{ __('Nama PIC') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="namaPic" type="text" class="form-control" 
+								value="{{ $booking['nama_pic'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="integraPic" class="col-md-4 col-form-label text-md-left">{{ __('User Integra PIC') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="integraPic" type="text" class="form-control" 
+								value="{{ $booking['integra_pic'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="emailPic" class="col-md-4 col-form-label text-md-left">{{ __('Email ITS PIC') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="emailPic" type="text" class="form-control" 
+								value="{{ $booking['email_pic'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="sivitas" class="col-md-4 col-form-label text-md-left">{{ __('Sivitas Akademika') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="sivitas" type="text" class="form-control" 
+								value="{{ $booking['sivitas'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="penyelengaraAcara" class="col-md-4 col-form-label text-md-left">{{ __('Penyelengara Acara') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="penyelengaraAcaraType" type="text" class="form-control" 
+								value="{{ $booking['org_type'] }}" disabled
+							>
+							<input 
+								id="penyelengaraAcara" type="text" class="form-control" 
+								value="{{ $booking['org_nama'] }}" disabled
+							>
+						</div>
+					</div>	
+
+					<div class="form-group row">
+						<label for="noWa" class="col-md-4 col-form-label text-md-left">{{ __('No. WA') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="noWa" type="tel" class="form-control"
+								value="{{ $booking['no_wa'] }}" disabled
+							>
+						</div>
+					</div>
+					@endif
+
+					<div class="form-group row">
+						<label for="namaAcara" class="col-md-4 col-form-label text-md-left">{{ __('Nama Acara') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="namaAcara" type="text" class="form-control" 
+								value="{{ $booking['nama_acara'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="unitDepartemen" class="col-md-4 col-form-label text-md-left">{{ __('Unit/Departemen') }}</label>
+						<i class="fa fa-building booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="unitDepartemen" type="text" class="form-control" 
+								value="{{ $booking['unit'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="waktuMulai" class="col-md-4 col-form-label text-md-left">{{ __('Waktu Mulai Webinar') }}</label>
+						<i class="fa fa-calendar-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="waktuMulai" type="datetime-local" class="form-control" 
+								value="{{ $booking['waktu_mulai'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="waktuSelesai" class="col-md-4 col-form-label text-md-left">{{ __('Waktu Selesai Webinar') }}</label>
+						<i class="fa fa-calendar booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="waktuSelesai" type="datetime-local" class="form-control" 
+								value="{{ $booking['waktu_akhir'] }}" onchange="onupdateWaktu()" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="durasi" class="col-md-4 col-form-label text-md-left">{{ __('Durasi Webinar') }}</label>
+						<i class="fa fa-clock-o booking"></i>
+						<div class="col-md-6">
+							<input id="durasi" type="text" class="form-control" value="" onchange="onupdateDurasi()" disabled> <div>jam</div>
+						</div>
+					</div>
+
+					@if($isOwner || $isAdmin)
+					<div class="form-group row">
+						<label for="namaAnda" class="col-md-4 col-form-label text-md-left">{{ __('Nama Anda') }}</label>
+						<i style="padding-left: 1px" class="fa fa-user booking"></i>
+						<!-- style="margin-left: 1px" -->
+						<div class="col-md-6">
+							<input 
+								id="namaAnda" type="text" class="form-control" 
+								value="{{ $booking['reg_nama'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="emailITS" class="col-md-4 col-form-label text-md-left">{{ __('Email ITS') }}</label>
+						<i class="fa fa-envelope-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="emailITS" type="email" class="form-control" 
+								value="{{ $booking['reg_email'] }}" disabled autocomplete="email"
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="userIntegra" class="col-md-4 col-form-label text-md-left">{{ __('User Integra') }}</label>
+						<i class="fa fa-address-card booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="userIntegra" type="text" class="form-control" 
+								value="{{ $booking['reg_integra'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="group" class="col-md-4 col-form-label text-md-left">{{ __('Group') }}</label>
+						<i class="fa fa-users booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="group" type="text" class="form-control" 
+								value="{{ $booking['group'] }}" disabled
+							>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="relayITSTV" class="col-md-4 col-form-label text-md-left">{{ __('Relay ke ITS TV') }}</label>
+						<i class="fa fa-sticky-note-o booking"></i>
+						<div class="col-md-6">
+							<input 
+								id="relayITSTV" type="text" class="form-control" 
+								value="{{ $booking['relay_ITSTV']?'iya':'tidak' }}" disabled
+							> 
+							<!-- <label for="iya">Iya</label>
+							<label for="tidak">Tidak</label> -->
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="pesertaBanyak" class="col-md-4 col-form-label text-md-left">{{ __('Peserta sebanyak 500 atau lebih') }}</label>
+						<i class="fa fa-sticky-note-o"></i>
+						<div class="col-md-6">
+							<input 
+								id="pesertaBanyak" type="text" class="form-control" 
+								value="{{ $booking['peserta_banyak']==false?'< 500':'500-1000' }}" disabled
+							>
+						</div>
+						{{-- <sub>Jawaban iya mengurangi kemungkinan di approve karena kurangnya sumber daya</sub> --}}
+					</div>
+
+					<div class="form-group row">
+						<label for="disetujui" class="col-md-4 col-form-label text-md-left">{{ __('Current Approval Status') }}</label>
+						<i class="fa fa-address-card"></i>
+						<div class="col-md-6">
+							<input 
+								id="disetujui" type="text" class="form-control" 
+								value="{{ null!==$booking['disetujui'] ? ($booking['disetujui']?'approved':'denied'):'' }}" disabled
+							>
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="alasan" class="col-md-4 col-form-label text-md-left">{{ __('Alasan') }}</label>
+						<i class="fa fa-sticky-note-o"></i>
+						<div class="col-md-6">
+							<textarea id="alasan" type="text" class="form-control" disabled> {{ $booking['deskripsi_disetujui'] }}</textarea>
+						</div>
+					</div>
+					@endif
+					@if($isAdmin)
+					<form method="POST" action="{{ route('booking.verify') }}">
+						@csrf
+						<input name="id" type="hidden" value="{{ $booking['id'] }}">
+						<input id="verify" name="verify" type="hidden" value="">
+
+						<div class="form-group row">
+							<label for="hostNama" class="col-md-4 col-form-label text-md-left">{{ __('Host Name') }}</label>
+							<i class="fa fa-building"></i>
+							<div class="col-md-6">
+								<input 
+									id="hostNama" type="text" class="form-control" name="hostNama" 
+									value="{{ old('hostNama')??$booking['api_host_nama'] }}" required
+								>
+							</div>
+						</div>
+
+						<div class="form-group row">
+							<label for="hostEmail" class="col-md-4 col-form-label text-md-left">{{ __('Host Email') }}</label>
+							<i class="fa fa-envelope-o"></i>
+							<div class="col-md-6">
+								<input 
+									id="hostEmail" type="email" class="form-control" name="hostEmail" 
+									value="{{ old('hostEmail')??$booking['api_host_email'] }}" required autocomplete="email"
+								>
+							</div>
+						</div>
+
+						<div class="form-group row">
+							<label for="alasan" class="col-md-4 col-form-label text-md-left">{{ __('Alasan') }}</label>
+							<i class="fa fa-sticky-note-o"></i>
+							<div class="col-md-6">
+								<textarea id="alasan" type="text" class="form-control" name="alasan">{{ old('alasan') }}</textarea>
+							</div>
+						</div>
+	
+						<div class="form-group row mb-0">
+							<div class="col-md-8 offset-md-4">
+								<button type="button" class="btn btn-submit" onclick="acceptBooking()">
+									{{ __('Approve Booking') }}
+								</button>
+								<button type="button" class="btn btn-outline-danger" onclick="denyBooking()">
+									{{ __('Deny Booking') }}
+								</button>
+							</div>
+						</div>
+					</form>
+					@endif
+				</div>
+			</div>
+		</div>
+	</div>
+
+@if($isAdmin)
+<script src="{{ asset('js/booking/verify.js') }}" defer></script>
+@endif
+<script src="{{ asset('js/booking/durasi.js') }}" defer></script>
+@endsection
