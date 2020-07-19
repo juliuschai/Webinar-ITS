8089d0fb71687031d8e01ecbacd1d58ddbd179a9
hipzi <zahramillah582@gmail.com>
2020-07-19 18:32:22 +0700

    add admin calendar

---
+++ resources/views/admin/calendar/calendar.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="right_col booking" role="main">
+    <div class="col-md-12 col-sm-12">
+    <h2 class="table-title">Jadwal Webinar</h2>
+        <div class="col-xl-12">
+            <!-- Calendar Container -->
+            <div class="js-calendar"></div>
+        </div>
+    </div>
+</div>
+
+@endsection
