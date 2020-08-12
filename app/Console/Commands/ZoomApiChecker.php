fee63acd2a076c21770ab0c71759625958484fe0
Julius <juliustjai20@gmail.com>
2020-08-12 10:13:53 +0700

    Zoom API ambiguous email update

--- app/Console/Commands/ZoomApiChecker.php
+++ app/Console/Commands/ZoomApiChecker.php
@@ -69,13 +69,13 @@ class ZoomApiChecker extends Command
         }
 
         // Bookings that will start soon
-        $booking_times = BookingTime::where('waktu_mulai', '<=', Carbon::now()->addMinutes(37))
+        $booking_times = BookingTime::where('booking_times.waktu_mulai', '<=', Carbon::now()->addMinutes(37))
             ->join('bookings', 'bookings.id', '=', 'booking_times.booking_id')
             ->where('bookings.disetujui', true)
             ->where('booking_times.disetujui', true)
             ->where('booking_times.status', 'pending')
             ->orderBy('booking_times.waktu_mulai', 'asc')
-            ->get('booking_times.*');
+            ->get();
 
         foreach ($booking_times as $booking_time) {
             // Make new password
